51e99de703c7e630a42ef4f5bb7cdc848e9af548
kiyanac <41528791+kiyanac@users.noreply.github.com>
2018-10-03 23:49:10 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ let restaurants,
 var newMap
 var markers = []
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+  .register('/js/sw.js')
+  .catch(function (err) {
+    console.error(err);
+
+  });
+}
+
 /**
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
@@ -78,7 +87,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiY2hlcnJ5ayIsImEiOiJjam1sOWQ1eTUwNXV0M3BtajVkemc4dnJ4In0.y6ZdClQTwFvq7GgkYWBGHQ',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
