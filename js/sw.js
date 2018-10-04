990891cfc145ffb5b3c68ec9058cbc8c781cb6bf
kiyanac <41528791+kiyanac@users.noreply.github.com>
2018-10-03 23:40:31 -0500

    Create sw.js

---
+++ js/sw.js
@@ -0,0 +1,57 @@
+console.log('Service Worker up');
+
+const cachedFiles = [
+  '/',
+  '/mws-restaurant-stage-1-master',
+  '/index.html',
+  '/restaurant.html',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/js/dbhelper.js',
+  '/css/styles.css',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll(cachedFiles);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      if(response) {
+        console.log(e.request 'in cache');
+        return response;
+      }
+      else {
+        console.log('Fetching' e.request);
+        return fetch(e.request)
+        .then(function(response) {
+          const clone = response.clone();
+          caches.open('v1').then(function(cache) {
+            cache.put(e.request, clone);
+          })
+          return response;
+        })
+        .catch(function(err) {
+          console.error(err);
+        });
+      }
+    })
+  );
+});
+
